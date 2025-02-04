b58c85566faad97fa9b84751d2b27af2341ec2d1
levired6 <levi.j.redinger@gmail.com>
2025-02-04 11:46:57 -0500

    Update server.js
    small fixes

--- server.js
+++ server.js
@@ -7,30 +7,33 @@ http.createServer((request, response) => {
     q = new URL(addr, 'http://' + request.headers.host),
     filePath = '';
 
-    fs.appendFile('log.txt', 'URL:' + addr + '\nTimestamp:' + new Date() + '\n\n' , (err) => {
+    fs.appendFile('log.txt', 'URL:' + addr + '\nTimestamp:' + new Date() + '\n\n', (err) => {
         if (err) {
-            console.log(err);
-        }else {
+            console.error(err);
+        } else {
             console.log('Added to log.');
         }
     });
 
     if (q.pathname.includes('documentation')) {
         filePath = (__dirname + '/documentation.html');
-    }else {
+    } else {
         filePath = 'index.html';
     }
 
     fs.readFile(filePath, (error, data) => {
-        if (err) {
-            throw err;
+        if (error) {
+            console.error("Error reading file:", error);
+            response.writeHead(500,{'Content_Type': 'text/plain'});
+            response.ent("Internal Server Error");
+            return;
         }
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(data);
     response.end();
 
-});
-
 }).listen(8080);
+
 console.log('My first Node test server is running on Port 8080.');
+});
