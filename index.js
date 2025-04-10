1323c4325c872e25c64db05c34809e914179594e
levired6 <levi.j.redinger@gmail.com>
2025-04-10 14:31:44 -0400

    Update port
    Changed port listening as the app keeps failing to open.

--- index.js
+++ index.js
@@ -376,7 +376,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+app.listen(port, () => {
+ console.log('Listening on Port' + port);
 });
 
