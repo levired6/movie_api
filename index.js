dd2ad56761648915af7af3bd80c72571988ba28c
levired6 <levi.j.redinger@gmail.com>
2025-04-15 15:39:31 -0400

    Update index.js
    Added cors language, changed key value on heroku config vars.

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
-const cors = require('cors');
 const { check, validationResult } = require('express-validator');//Import express-validator
 
 //mongoose.connect('mongodb://localhost:27017/cfdb');{useNewUrlParser: true, useUnifiedTopology: true};//Connect to MongoDB database
@@ -23,8 +22,9 @@ app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(cors());
 
+const cors = require('cors');
+app.use(cors());
 let auth = require('./auth')(app);//Imports the auth.js file to create the endpoint for login
 const passport =require('passport'); 
 require('./passport');//Passport module to import the passport.js file into the project
@@ -34,103 +34,18 @@ app.get('/', (req, res) => {
   res.send(`Welcome to myFlix app! Here are the top 10 2025 Oscar nomonated movies!`);
 });
 
-/*
-let movies = [
-    {
-      "Title": "Anora",
-      "Discription": "A young escort from Brooklyn meets and impulsively marries the son of a Russian oligarch. Once the news reaches Russia, her fairy tale is threatened as his parents set out for New York to get the marriage annulled.",
-      "Genre": { "Name": "Comedy, Drama, Romance"},
-      "Director": {"Name": "Sean Baker"},
-      "ImageURL": "images/Anora.jpg",
-    },
+const allowedOrigins = ['http://localhost:8080', 'https://oscars2025-f0070acec0c4.herokuapp.com/'];
 
-    { 
-        "Title": "The Brutalist",
-        "Discription": "When a visionary architect and his wife flee post-war Europe in 1947 to rebuild their legacy and witness the birth of modern United States, their lives are changed forever by a mysterious, wealthy client.",
-        "Genre":{ "Name": "Drama"},
-        "Director": {"Name": "Brady Corbet",},
-        "ImageURL": "images/The Brutalist.jpg",
-      },
-
-      { 
-        "Title": "A Complete Unknown",
-        "Discription": "Set in the influential New York City music scene of the early 1960s, A Complete Unknown follows 19-year-old Minnesota musician Bob Dylan's meteoric rise as a folk singer to concert halls and the top of the charts as his songs and his mystique become a worldwide sensation that culminates in his groundbreaking electric rock-and-roll performance at the Newport Folk Festival in 1965.",
-        "Genre": { "Name": "Biography, Drama, Music"},
-        "Director": {"Name": "James Mangold"},
-        "ImageURL": "images/A Complete Unknown.jpg"
-      },
-
-      { 
-        "Title": "Conclave",
-        "Discription": "When Cardinal Lawrence is tasked with leading one of the world's most secretive and ancient events, selecting a new Pope, he finds himself at the center of a web of conspiracies and intrigue that could shake the very foundation of the Catholic Church.",
-        "Genre": {"Name": "Drama, Mystery, Thriller"},
-        "Director": {"Name": "Edward Berger"},
-        "ImageURL": "images/Conclave.jpg"
-      },
-
-      { 
-        "Title": "Dune: Part Two",
-        "Discription": "Paul Atreides unites with the Fremen while on a warpath of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the universe, he endeavors to prevent a terrible future.",
-        "Genre": {"Name": "Action, Adventure, Drama, Sci-Fi"},
-        "Director": {"Name": "Denis Villeneuve"},
-        "ImageURL": "images/Dune2.jpg"
-      },
-
-      { 
-        "Title": "Emilia Pérez",
-        "Discription": "Emilia Pérez follows three remarkable women in Mexico, each pursuing their own happiness. Cartel leader Emilia enlists unappreciated lawyer Rita to help fake her death so that she can finally live authentically as her true self.",
-        "Genre": {"Name": "Comedy, Crime, Drama, Musical, Thriller"},
-        "Director": {"Name": "Jacques Audiard"},
-        "ImageURL": "images/Emilia Perez.jpg"
-      },
-
-      { 
-        "Title": "I'm Still Here",
-        "Discription": "A mother is forced to reinvent herself when her family's life is shattered by an act of arbitrary violence during the tightening grip of a military dictatorship in Brazil, 1971.",
-        "Genre": {"Name": "Biography, Drama, History"},
-        "Director": {"Name": "Walter Salles"},
-        "ImageURL": "images/I'm Still Here.jpg"
-      },
-
-      { 
-        "Title": "Nickel Boys",
-        "Discription": "Based on the Pulitzer Prize-winning novel by Colson Whitehead, Nickel Boys chronicles the powerful friendship between two young African-American men navigating the harrowing trials of reform school together in Florida.",
-        "Genre": {"Name": "Drama"},
-        "Director": {"Name": "RaMell Ross"},
-        "ImageURL": "images/Nickel Boys.jpg"
-      },
-
-      { 
-        "Title": "The Substance",
-        "Discription": "A fading celebrity takes a black-market drug: a cell-replicating substance that temporarily creates a younger, better version of herself.",
-        "Genre": {"Name": "Drama, Horror, Sci-Fi"},
-        "Director": {"Name": "Coralie Fargeat"},
-        "ImageURL": "images/The Substance.jpg"
-      },
-
-      { 
-        "Title": "Wicked",
-        "Discription": "Elphaba, a misunderstood young woman because of her green skin, and Galinda, a popular girl, become friends at Shiz University in the Land of Oz. After an encounter with the Wonderful Wizard of Oz, their friendship reaches a crossroads.",
-        "Genre": {"Name": "Fantasy, Musical, Romance"},
-        "Director": {"Name": "Jon M. Chu"},
-        "ImageURL": "images/Wicked.jpg"
-      },
-];
-let users = [
-  {
-    "id": 1,
-    "name": "Kim",
-    "email": "kim@example.com",
-    "favoriteMovies": ["Wiked", "The Substance"]
-  },
-  {
-    "id": 2,
-    "name": "Joe",
-    "email": "joe@example.com",
-    "favoriteMovies": ["Anora"]
-  },
-]; //Users array data
-*/
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
   //READ all movies
   app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
@@ -378,6 +293,6 @@ app.delete('/users/:username/movies/:MovieID', passport.authenticate('jwt', { se
 });
 */
 const port = process.env.PORT || 8080;
-  app.listen(port, () => {
-   console.log('Listening on Port ' + port);
-  });
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
+});
